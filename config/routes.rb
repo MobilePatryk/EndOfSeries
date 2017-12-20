a25025e3ea6fbc56515e5fdd021fa1dbf8ff2bab
= <patryk.dabrowski@decathlon.com>
2017-12-20 16:36:48 +0000

    #1 fixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,17 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
 
   get 'home/show'
+  
+  GoogleAuthExample::Application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+
+  root to: "home#show"
+end
 
   root :to => 'ends#index'
   require 'sidekiq/web'
