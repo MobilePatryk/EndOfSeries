182f692afb1489ac27a21d2ef108f81b868a6fec
= <patryk.dabrowski@decathlon.com>
2017-12-20 23:15:39 +0000

    Add creditentials for API to ENV vars

--- config/initializers/omniatuh.rb
+++ config/initializers/omniatuh.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "915735438771-66k6hpoc907cl2ftnn0pig6vagomtgff.apps.googleusercontent.com", "W-D9G9XhHpYRWhbm59hykEWn"
+  provider :google_oauth2, ENV['OACLIENT'], ENV['OASECRET']
 end
