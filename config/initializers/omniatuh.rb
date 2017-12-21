423c6895240a2688df654595771412c2761e97ba
= <patryk.dabrowski@decathlon.com>
2017-12-21 10:35:18 +0000

    Add some env restrictions #1

--- config/initializers/omniatuh.rb
+++ config/initializers/omniatuh.rb
@@ -1,4 +1,14 @@
 OmniAuth.config.logger = Rails.logger
+if Rails.env.production?
+    
+    
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['OACLIENT'], ENV['OASECRET'],{
+      :hd => 'decathlon.com'
+  }
+  end
+  
+else
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, "915735438771-ee0msacribn2n52lqb8mge1p51pt7aii.apps.googleusercontent.com", "vWhIHikeAKL03PSCCciQAkGN",{
@@ -6,10 +16,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   }
 end
 
-  if Rails.env.production?
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['OACLIENT'], ENV['OASECRET'],{
-      :hd => 'decathlon.com'
-  }
-  end
-  end
+end
+
+  
